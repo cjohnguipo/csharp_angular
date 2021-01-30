5b3f2597dc5e46f5218e286b2d1ebe0b20682f69
Christian John Guipo <cjohnguipo@gmail.com>
2021-01-30 16:11:02 +0800

    End of Section 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,21 @@ export class AppComponent implements OnInit {
 
 
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
-
+    this.checkBasket();
   }
+
+  private checkBasket() {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId){
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('Basket Initialized ...');
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
 }
